f62545c377d5ca7d427946c88b526f5ab06be09e
Wenzhou Mei <wenzhoumei7@gmail.com>
2025-08-21 20:38:57 +0100

    [OR1K] Implement getting relocation types.

--- llvm/lib/Target/OR1K/MCTargetDesc/OR1KELFObjectWriter.cpp
+++ llvm/lib/Target/OR1K/MCTargetDesc/OR1KELFObjectWriter.cpp
@@ -8,17 +8,15 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include "MCTargetDesc/OR1KFixupKinds.h"
+#include "MCTargetDesc/OR1KMCAsmInfo.h"
 #include "MCTargetDesc/OR1KMCTargetDesc.h"
-#include "llvm/ADT/STLExtras.h"
-#include "llvm/BinaryFormat/ELF.h"
+#include "llvm/MC/MCContext.h"
 #include "llvm/MC/MCELFObjectWriter.h"
-#include "llvm/MC/MCExpr.h"
 #include "llvm/MC/MCFixup.h"
 #include "llvm/MC/MCObjectWriter.h"
 #include "llvm/MC/MCValue.h"
 #include "llvm/Support/ErrorHandling.h"
-#include <cassert>
-#include <cstdint>
 
 using namespace llvm;
 
@@ -32,7 +30,6 @@ public:
 protected:
   unsigned getRelocType(const MCFixup &, const MCValue &,
                         bool IsPCRel) const override;
-  bool needsRelocateWithSymbol(const MCValue &, unsigned Type) const override;
 };
 } // namespace
 
@@ -42,18 +39,26 @@ OR1KObjectWriter::OR1KObjectWriter(uint8_t OSABI)
 
 OR1KObjectWriter::~OR1KObjectWriter() {}
 
+
 unsigned OR1KObjectWriter::getRelocType(const MCFixup &Fixup,
-                                          const MCValue &Target,
-                                          bool IsPCRel) const {
-  report_fatal_error("invalid fixup kind!");
+                                        const MCValue &Target,
+                                        bool IsPCRel) const {
+  switch ((unsigned)Fixup.getKind()) {
+  default:
+    reportError(Fixup.getLoc(), "unsupported relocation type");
+    return ELF::R_OR32_NONE;
+  case FK_Data_4:
+    return ELF::R_OR32_32;
+  case OR1K::fixup_or1k_hi16:
+    return ELF::R_OR32_CONSTH;
+  case OR1K::fixup_or1k_lo16:
+    return ELF::R_OR32_CONST;
+  case OR1K::fixup_or1k_branch:
+    return ELF::R_OR32_JUMPTARG;
+  }
 }
 
 std::unique_ptr<MCObjectTargetWriter>
 llvm::createOR1KObjectWriter(uint8_t OSABI, bool IsLittleEndian) {
   return std::make_unique<OR1KObjectWriter>(OSABI);
-}
-
-bool OR1KObjectWriter::needsRelocateWithSymbol(const MCValue &,
-                                                 unsigned Type) const {
-  return false;
 }
